9a8445da9df5e1a556f858d17a74343fd4e12246
Thomas <tfnielson.se@gmail.com>
2022-12-12 23:30:32 -0700

    commit tree 4e232326335fd35eaa348a18d13210858bf6895c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun (activity = `roller-skate`) {
+    let display = `This Saturday, I want to ${activity}!`;
+    return display;
+}
+saturdayFun;
+saturdayFun(`bathe my dog`);
+
+function mondayWork (activity = `go to the office`) {
+    let display = `This Monday, I will ${activity}.`
+    return display;
+}
+
+mondayWork;
+mondayWork(`work from home`);
+
+function wrapAdjective (ish = `*`) {
+    return function (adj = `special`) {
+        return `You are ${ish}${adj}${ish}!`
+    }
+}
